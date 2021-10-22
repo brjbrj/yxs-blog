7d8ce90cc029cab5f382e12bda1d37aa557b8903
yingxiaoshu <82161965+yingxiaoshu@users.noreply.github.com>
2021-10-22 19:28:50 +0800

    Delete admin directory

--- admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	$_SESSION['adminlogin']=0;
-	if($_SESSION['adminlogin']!=1)
-	{
-		@header('Content-Type: text/html; charset=UTF-8');
-		exit("<script language='javascript'>alert('您已退出登录！');window.location.href='login.php';</script>");
-	}
-?>
