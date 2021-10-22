6f6f415dfb2e25df0389ec95a9f07c2be36dc106
yingxiaoshu <82161965+yingxiaoshu@users.noreply.github.com>
2021-10-22 19:29:51 +0800

    Delete user directory

--- user/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	$_SESSION['islogin']=0;
-	if($_SESSION['islogin']!=1)
-	{
-		@header('Content-Type: text/html; charset=UTF-8');
-		exit("<script language='javascript'>alert('您已退出登录！');window.location.href='../login.php';</script>");
-	}
-?>
